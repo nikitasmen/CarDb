8fd4c05ede6a4324c556f118273355ffd2d6b6b4
nikmen <menounosnikitas@gmail.com>
2025-01-25 00:39:09 +0200

    fix(cliApp):installer

--- cli.py
+++ cli.py
@@ -6,28 +6,6 @@ class Cli:
     def __init__(self):
         self.tracker = CarTracker()
         
-    def run(self): 
-        while True:
-            command = input("Enter command: ")
-            if command == "exit":
-                break
-            self.process_command(command)
-
-    def process_command(self, command): 
-        if command == "--help":
-            self.print_help()
-        elif command == "add":
-            self.add_car()
-        elif command == "display":
-            self.display_all_cars()
-        elif command == "search":
-            self.search_car()
-        elif command == "delete":
-            self.delete_car()
-        else:
-            print("Invalid command. Type '--help' to see the available commands.")
-
-
     def add_car(self, modelname, manufacturer, year, origincountry, category, modelmanufact, more):
         """Add a new car to the database"""
         self.tracker.addData(modelname, manufacturer, year, origincountry, category, modelmanufact, more)
@@ -54,6 +32,8 @@ class Cli:
         self.tracker.deleteData(modelname)
         click.echo(f"Car '{modelname}' deleted successfully!")
 
+# Create an instance of the Cli class for library use
+cli_instance = Cli()
 
 @click.group()
 def cli():
@@ -61,12 +41,12 @@ def cli():
     pass
 
 @click.command()
-@click.option('--modelName', prompt="Model Name", help="The model of the car")
+@click.option('--modelname', prompt="Model Name", help="The model of the car")
 @click.option('--manufacturer', prompt="Manufacturer", help="The car's manufacturer")
-@click.option('--year', prompt="Year", help="Year of manufacture")
-@click.option('--originCountry', prompt="Origin Country", help="Country of origin")
+@click.option('--year', prompt="Year", type=int, help="Year of manufacture")
+@click.option('--origincountry', prompt="Origin Country", help="Country of origin")
 @click.option('--category', prompt="Category", help="Car category")
-@click.option('--modelManufact', prompt="Model Manufacturer", help="Model manufacturing details")
+@click.option('--modelmanufact', prompt="Model Manufacturer", help="Model manufacturing details")
 @click.option('--more', prompt="More Info (URL)", help="Additional info link")
 def add(modelname, manufacturer, year, origincountry, category, modelmanufact, more):
     """Add a new car"""
@@ -78,19 +58,16 @@ def display():
     cli_instance.display_all_cars()
 
 @click.command()
-@click.option('--modelName', prompt="Enter Model Name", help="Model to search")
+@click.option('--modelname', prompt="Enter Model Name", help="Model to search")
 def search(modelname):
     """Search for a car"""
     cli_instance.search_car(modelname)
 
 @click.command()
-@click.option('--modelName', prompt="Enter Model Name", help="Model to delete")
+@click.option('--modelname', prompt="Enter Model Name", help="Model to delete")
 def delete(modelname):
     """Delete a car"""
     cli_instance.delete_car(modelname)
-
-# Create an instance of the Cli class
-cli_instance = Cli()
 
 # Add commands to the CLI group
 cli.add_command(add)
@@ -98,3 +75,6 @@ cli.add_command(display)
 cli.add_command(search)
 cli.add_command(delete)
 
+# Allow running the script directly
+if __name__ == "__main__":
+    cli()
